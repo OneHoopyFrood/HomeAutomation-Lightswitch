d8fd5e0546a2f62d802571be2747a5774b3c1059
Cole Panike <colepanike@gmail.com>
2019-11-03 12:20:54 -0600

    Don't set wifi every boot, check for connection first.

--- boot.py
+++ boot.py
@@ -7,19 +7,27 @@ def startWebRepl():
     import webrepl
     webrepl.start()
 
+# Only need to be called once, the ESP8266 will recall the network
 def do_connect():
     import network
-    import wifi_details
+    import time
     sta_if = network.WLAN(network.STA_IF)
-    ap_if = network.WLAN(network.AP_IF)
+
+    #Wait to see if connection forms
+    for i in range(0,3):
+        if sta_if.isconnected():
+            break
+        time.sleep_ms(600)
+
     if not sta_if.isconnected():
-        print('connecting to network...')
+        import wifi_details
+        print('Connecting to %s...' %(wifi_details.ssid))
         sta_if.active(True)
         sta_if.connect(wifi_details.ssid, wifi_details.password)
         while not sta_if.isconnected():
             pass
-    ap_if.active(False)
-    print('network config:', sta_if.ifconfig())
+    network.WLAN(network.AP_IF).active(False)
+    print('Network config:', sta_if.ifconfig())
 
-startWebRepl()
+# startWebRepl()
 do_connect()
