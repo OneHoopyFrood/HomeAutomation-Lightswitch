2677a9d0c892f3f2c2612779ed83370c8846b7a9
Cole Panike <colepanike@gmail.com>
2019-11-04 23:34:19 -0600

    Add message retention on state topic, specify qos

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ def set_light_state(state):
 	global mqttClient
 	led = Pin(0, Pin.OUT)
 	led.value(state)
-	print('Light: ', bool(state))
+	print('Pin: ', bool(state))
 	# Now publish the state on the state topic
-	mqttClient.publish(mqttConfig['state_topic'], b'1' if state else b'0')
+	mqttClient.publish(mqttConfig['state_topic'], b'1' if state else b'0', True, 1)
 
 def handle_msg(topic, msg):
 	try:
@@ -18,13 +18,15 @@ def handle_msg(topic, msg):
 		print("Message not a number")
 
 def main():
+	# TODO I'm pretty sure globals are still evil in MicroPython. Need a better solution
 	global mqttClient
 	mqttClient = MQTTClient(mqttConfig['client_id'], mqttConfig['server'])
 
 	# Setup the conneciton
 	mqttClient.set_callback(handle_msg)
 	mqttClient.connect()
-	mqttClient.subscribe(mqttConfig['set_topic'])
+	mqttClient.set_last_will(mqttConfig['state_topic'], b'0', True) # Last Will message is that the light is now off.
+	mqttClient.subscribe(mqttConfig['set_topic'], 1)
 	print("Connected to %s, subscribed to %s topic" % (mqttConfig['server'], mqttConfig['set_topic']))
 
 	# Off on reboot
