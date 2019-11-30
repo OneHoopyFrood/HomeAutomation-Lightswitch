164313cbfb020a4d4c705c51f8431d522dc16895
Cole Panike <colepanike@gmail.com>
2019-11-30 17:54:30 -0600

    Add led indicator for connection

--- boot.py
+++ boot.py
@@ -11,13 +11,18 @@ def startWebRepl():
 def do_connect():
     import network
     import time
+    from machine import Pin, Signal
     sta_if = network.WLAN(network.STA_IF)
 
+    # LED is used to indicate connection state
+    led = Signal(Pin(13, Pin.OUT), invert=True)
+    led.value(0)
+
     #Wait to see if connection forms
-    for i in range(0,3):
+    for i in range(0,4):
         if sta_if.isconnected():
             break
-        time.sleep_ms(600)
+        time.sleep_ms(500)
 
     if not sta_if.isconnected():
         from config import wifi
@@ -27,7 +32,8 @@ def do_connect():
         while not sta_if.isconnected():
             pass
     network.WLAN(network.AP_IF).active(False)
+    led.value(1)
     print('Network config:', sta_if.ifconfig())
 
-# startWebRepl()
 do_connect()
+startWebRepl()
